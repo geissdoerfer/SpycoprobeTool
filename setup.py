850d5942645d407919445b00da79ba4ad53d6599
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2023-05-02 15:30:34 +0200

    bumpversion 1.0.3 --> 1.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="spycoprobe",
-    version="1.0.2",
+    version="1.0.3",
     description="Tool for controlling Spycoprobe SBW debugger",
     long_description=README,
     long_description_content_type="text/markdown",
